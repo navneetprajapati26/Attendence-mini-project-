3de96d76f44657b6e9a70bf8bcc476425edc035c
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-11-24 14:17:58 +0530

    getX add

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(title: Text('ATTENDENCE')),
+      appBar: AppBar(title: Text('Home')),
       body: Center(
         child: Column(
           children: [
